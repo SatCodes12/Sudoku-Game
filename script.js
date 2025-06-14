9f0942001cc5062d7694cb6478e05d1da2d248be
Satyaki Banerjee <banerj.satyaki@gmail.com>
2025-06-14 12:37:53 +0530

    Puzzles added

--- script.js
+++ script.js
@@ -24,22 +24,22 @@ const puzzleList = [
         [7, 0, 3, 0, 1, 8, 0, 0, 0]
     ],
     [
-        [1, 0, 0, 4, 8, 9, 0, 0, 6],
-        [7, 3, 0, 0, 0, 0, 0, 4, 0],
-        [0, 0, 0, 0, 0, 1, 2, 9, 5],
-        [0, 9, 0, 0, 0, 4, 0, 7, 0],
-        [8, 4, 0, 0, 0, 0, 0, 0, 0],
-        [0, 6, 7, 0, 9, 0, 0, 0, 0],
-        [0, 0, 8, 7, 0, 6, 0, 0, 0],
-        [0, 0, 0, 9, 0, 0, 4, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        [0, 0, 3, 0, 2, 0, 6, 0, 0],
+        [9, 0, 0, 3, 0, 5, 0, 0, 1],
+        [0, 0, 1, 8, 0, 6, 4, 0, 0],
+        [0, 0, 8, 1, 0, 2, 9, 0, 0],
+        [7, 0, 0, 0, 0, 0, 0, 0, 8],
+        [0, 0, 6, 7, 0, 8, 2, 0, 0],
+        [0, 0, 2, 6, 0, 9, 5, 0, 0],
+        [8, 0, 0, 2, 0, 3, 0, 0, 9],
+        [0, 0, 5, 0, 1, 0, 3, 0, 0]
     ],
     [
         [6, 0, 0, 8, 2, 7, 5, 0, 9],
         [2, 5, 0, 0, 4, 0, 0, 3, 0],
-        [0, 0, 8, 0, 0, 1, 7, 0, 2],
+        [0, 8, 0, 0, 0, 1, 0, 7, 2],
         [0, 0, 2, 4, 0, 0, 0, 0, 7],
-        [0, 6, 0, 7, 5, 0, 0, 8, 0],
+        [0, 0, 6, 7, 5, 0, 0, 8, 0],
         [7, 4, 5, 2, 1, 0, 0, 0, 0],
         [5, 6, 1, 0, 7, 0, 9, 2, 8],
         [4, 0, 0, 0, 0, 0, 0, 0, 3],
@@ -162,7 +162,7 @@ for (let i = 1; i <= 9; i++) {
                     clearInterval(timer);
                     isRunning = false;
                     gameStarted = false;
-                    $('#game-msg').text(`Hurrah, you have solved it! Time taken - ${formatTime(seconds)}. Click Reset to play a new game.`);
+                    $('#game-msg').text(`Hurrah, you have solved it! Time taken - ${formatTime(seconds)}. Click New Game to play a new game.`);
                 }
             }
 
@@ -237,6 +237,7 @@ $resetYes.click(function () {
     updateDisplay();
     isRunning = false;
     gameStarted = false;
+    sudokuSolved = false;
     $('#game-msg').text('New game. Press Play to start again.');
 
     closeResetAllMenu();
